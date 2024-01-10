03057af664062084ac21586740e528e04bf7f520
AdarshVarma9395 <adarshvarma1999@gmail.com>
2024-01-10 07:25:58 +0530

    Templates
    created some bacis templates named as index.html and I've passed data through context..

--- home/views.py
+++ home/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("hey im a django server.")
+
+    peoples = [
+        {"Name" : "Adarsh varma" , "Age" : 24},
+        {"Name" : "Nitin Sirsath" , "Age" : 23},
+        {"Name" : "Swaraj Chandu" , "Age" : 25},
+        {"Name" : "Mitanshu Salvi" , "Age" : 26},
+        {"Name" : "Anurag Varma" , "Age" : 21},
+    ]
+
+    return render(request,"index.html", context = {'peoples' : peoples})
